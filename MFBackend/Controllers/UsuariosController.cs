5fe68ea11f78f0fa990f4c87150f14a90f1e38d5
Alycia Oliveira <1alycia.oliveira@gmail.com>
2024-10-13 01:07:23 -0600

    adicionando Bcrypt

--- MFBackend/Controllers/UsuariosController.cs
+++ MFBackend/Controllers/UsuariosController.cs
@@ -31,6 +31,7 @@ public class UsuariosController : Controller
     {
         if (ModelState.IsValid)
         {
+            usuario.Senha = BCrypt.Net.BCrypt.HashPassword(usuario.Senha);
             _context.Add(usuario);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
@@ -63,6 +64,7 @@ public class UsuariosController : Controller
         {
             try
             {
+                usuario.Senha = BCrypt.Net.BCrypt.HashPassword(usuario.Senha);
                 _context.Update(usuario);
                 await _context.SaveChangesAsync();
             }
